9cc7bc758608bef3893fb51acfc61d06250a1e3e
IVadim23 <inazrski@gmail.com>
2024-08-18 01:21:53 +0300

    Add files via upload

---
+++ 01. Sum Seconds/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01._Sum_Seconds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Прочитаме времет на всеки от състезателите
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+
+            //2. Намираме общото време
+            int time = first + second + third;
+
+            //3. Намираме това колко минути и колко секунди са
+            int min = time / 60; //колко пъти 60 се съдържа в time 
+            int sec = time % 60; //колко пъти 60 НЕ СЕ съдържа в time
+
+            //4. Отпечатваме резултата
+            //   => Секундите да се изведат с водеща нула (2=>"02", 17=>"17")
+            if (sec >= 10)
+            {
+                Console.WriteLine($"{min}:{sec}");
+            }
+            else
+            {
+                Console.WriteLine($"{min}:0{sec}");
+            }
+        }
+    }
+}
