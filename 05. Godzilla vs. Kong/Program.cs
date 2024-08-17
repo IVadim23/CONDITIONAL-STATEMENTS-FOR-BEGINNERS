4b609bfd3ad9ecf56b7fb4c7071b3d7d0354b69d
IVadim23 <inazrski@gmail.com>
2024-08-18 01:25:58 +0300

    Add files via upload

---
+++ 05. Godzilla vs. Kong/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _05._Godzilla_vs._Kong
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Budgetforthemovie = double.Parse(Console.ReadLine());
+            double Numberofextras = double.Parse(Console.ReadLine());
+            double Priceforclothingofonestatis = double.Parse(Console.ReadLine());
+            double Amountfordecor = Budgetforthemovie * 0.10;
+
+
+            if (Numberofextras <= 150)
+            {
+                double Amountforclothing =(Numberofextras * Priceforclothingofonestatis) ;
+                double Totalforthefilm = Amountforclothing + Amountfordecor;
+                if (Totalforthefilm <= Budgetforthemovie)
+                {
+                    Console.WriteLine("Action!");
+                    Console.WriteLine($"Wingard starts filming with {Totalforthefilm - Budgetforthemovie:F2} leva left. ");
+                }
+                else
+                {
+
+                    Console.WriteLine("Not enough money!");
+                    Console.WriteLine($"Wingard needs {Budgetforthemovie- Totalforthefilm:F2} leva more.");
+                }
+
+
+            }
+            else
+            {
+                double Amountforclothing = ((Numberofextras * Priceforclothingofonestatis)*0.10);
+                double Totalforthefilm = Amountforclothing + Amountfordecor;
+                double a = Budgetforthemovie - Totalforthefilm;
+                if (Totalforthefilm <= Budgetforthemovie)
+                {
+                    Console.WriteLine("Action!");
+                    Console.WriteLine($"Wingard starts filming with {Totalforthefilm - Budgetforthemovie:F2} leva left. ");
+                }
+                else 
+                        {
+
+                    Console.WriteLine("Not enough money!");
+                    Console.WriteLine($"Wingard needs {Budgetforthemovie - Totalforthefilm:F2} leva more.");
+                }
+            }  
+            
+            
+          
+
+
+
+        }
+    }
+}
